60c0ba6f299029c6325740ac961fc579e972dc5d
neo <arklove@qq.com>
2020-05-08 13:51:38 +0800

    chore: @semantic-release/git add package file

--- release.config.js
+++ release.config.js
@@ -19,7 +19,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md'],
+        assets: ['CHANGELOG.md', 'package.json'],
       },
     ],
     '@semantic-release/github',
