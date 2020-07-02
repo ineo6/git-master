5522df409983168a5e354bd31654a39a1e534a91
neo <arklove@qq.com>
2020-07-02 21:47:36 +0800

    fix: change sidebar layout event wide screen

--- src/common/adapters/github.js
+++ src/common/adapters/github.js
@@ -11,7 +11,7 @@ import * as githubDetect from './pageDetect/github';
 // Therefore, the below selector uses many path but only points to the same <main> element
 const GH_PJAX_CONTAINER_SEL = '#js-repo-pjax-container, div[itemtype="http://schema.org/SoftwareSourceCode"] main, [data-pjax-container]';
 
-const GH_CONTAINERS = '.container, .container-lg, .container-xl, .container-responsive';
+const GH_CONTAINERS = '.pagehead > nav, .container, .container-lg, .container-xl, .container-responsive';
 const GH_HEADER = '.js-header-wrapper > header';
 const GH_MAX_HUGE_REPOS_SIZE = 50;
 const GH_HIDDEN_RESPONSIVE_CLASS = '.d-none';
@@ -134,23 +134,22 @@ class GitHub extends PjaxAdapter {
 
     const autoMarginLeft = ($(document).width() - $containers.width()) / 2;
     const shouldPushEverything = sidebarPinned && sidebarVisible;
-    const smallScreen = autoMarginLeft <= sidebarWidth + SPACING;
 
     const direction = isSidebarLeft ? 'left' : 'right';
 
-    $('html').css({
-      [`margin-${direction}`]: shouldPushEverything && smallScreen ? sidebarWidth : '',
-      [`margin-${direction === 'left' ? 'right' : 'left'}`]: '',
-    });
-    $containers.css({
-      [`margin-${direction}`]: shouldPushEverything && smallScreen ? SPACING : '',
-      [`margin-${direction === 'left' ? 'right' : 'left'}`]: '',
-    });
+    if (shouldPushEverything) {
+      $('html').css({
+        [`margin-${direction}`]: sidebarWidth,
+        [`margin-${direction === 'left' ? 'right' : 'left'}`]: '',
+      });
 
-    if (shouldPushEverything && !smallScreen) {
-      // Override important in Github Header class in large screen
-      $header.attr('style', `padding-left: ${sidebarWidth + SPACING}px !important`);
+      // $header.attr('style', `padding-left: ${sidebarWidth + SPACING - autoMarginLeft}px !important`);
     } else {
+      $('html').css({
+        [`margin-${direction}`]: '',
+        [`margin-${direction === 'left' ? 'right' : 'left'}`]: '',
+      });
+
       $header.removeAttr('style');
     }
   }
