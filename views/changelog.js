78d4c50c695accf32a3837434a17adcfec5faa23
neo <arklove@qq.com>
2021-02-17 21:32:31 +0800

    docs: update changelog.js

--- views/changelog.js
+++ views/changelog.js
@@ -1,29 +1,12 @@
 export default {
-  feature: [
+  feature: [],
+  fix: [
     {
       text: {
-        en: 'Update GitHub dark mode style',
-        zh_CN: '更新黑暗模式样式',
+        en: 'Fix GM svg file size problem.',
+        zh_CN: '修复GM图标尺寸过大显示问题',
       },
-    },
-    {
-      text: {
-        en: 'Add option for official dark mode',
-        zh_CN: '为GitHub官方黑暗模式添加配置按钮',
-      },
-      description: {
-        en: 'see "Extension DarkMode" in Settings',
-        zh_CN: '请看"设置=>内置黑暗模式"',
-      },
-    },
-    {
-      text: {
-        en: 'Load entry tree for Gitee again',
-        zh_CN: 'Gitee再次启用完整加载，停止强制懒加载',
-      },
-      description: {},
     },
   ],
-  fix: [],
-  version: '1.11.6',
+  version: '1.12.1',
 };
