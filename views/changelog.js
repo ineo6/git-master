b019acc73fb1213de96ef9e02921f5373c447803
neo <arklove@qq.com>
2021-04-08 11:51:26 +0800

    docs: update changelog

--- views/changelog.js
+++ views/changelog.js
@@ -1,12 +1,23 @@
 export default {
-  feature: [],
-  fix: [
+  feature: [
     {
       text: {
-        en: 'Avoid multi-file downloaded by one click',
-        zh_CN: '修复某些情况下同时下载多个文件的问题',
+        en: 'Download acceleration for GitHub clone、releases、tags',
+        zh_CN: 'GitHub支持clone和releases页面下载加速',
+      },
+      description: {
+        en: 'see "GitHub download acceleration" in extension options，Only required by users in China',
+        zh_CN: '请看扩展的"选项=>GitHub下载加速"配置',
       },
     },
   ],
-  version: '1.12.2',
+  fix: [
+    {
+      text: {
+        en: 'Use default branch when viewing releases or tags',
+        zh_CN: 'releases和tags界面使用项目默认分支',
+      },
+    },
+  ],
+  version: '1.13.0',
 };
