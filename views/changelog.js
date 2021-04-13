12a351218095f35c378c4091439d1b88e44eb70b
neo <arklove@qq.com>
2021-04-13 18:37:31 +0800

    docs: 更新changelog

--- views/changelog.js
+++ views/changelog.js
@@ -2,28 +2,18 @@ export default {
   feature: [
     {
       text: {
-        en: 'Support GitHub more dark theme and auto mode',
-        zh_CN: '兼容GitHub新增的黑暗模式主题',
-      },
-    },
-    {
-      text: {
-        en: 'Download acceleration for GitHub clone、releases、tags',
-        zh_CN: 'GitHub支持clone和releases下载加速',
-      },
-      description: {
-        en: 'see "GitHub download acceleration" in extension options. Only required by users in China',
-        zh_CN: 'Zip下载、releases下载、tags下载以及http clone都可以加速，请在扩展的"选项=>GitHub下载加速"查看并启用',
+        en: 'Improve GitHub mirror feature',
+        zh_CN: '优化GitHub镜像加速功能',
       },
     },
   ],
   fix: [
     {
       text: {
-        en: 'Use default branch when viewing releases or tags',
-        zh_CN: 'releases和tags界面使用项目默认分支',
+        en: 'Fix GitHub notifications problem',
+        zh_CN: '修复GitHub通知问题',
       },
     },
   ],
-  version: '1.13.0',
+  version: '1.14.0',
 };
