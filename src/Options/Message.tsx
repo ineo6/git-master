249d1dc4bd6c48f5a8d048bfcf46a313d0655007
neo <arklove@qq.com>
2021-05-09 15:31:24 +0800

    feat: sanitize html string by dompurify

--- src/Options/Message.tsx
+++ src/Options/Message.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { browser } from 'webextension-polyfill-ts';
+import DOMPurify from 'dompurify';
 
-export default function Message({ i18n }) {
+export default function Message({ i18n }: { i18n: string }) {
   const text = browser.i18n.getMessage(i18n);
 
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: text,
+        __html: DOMPurify.sanitize(text),
       }}
     />
   );
