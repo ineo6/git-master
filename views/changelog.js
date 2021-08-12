60958b79efe6691c59551c99b5aa1907d2d26ad2
neo <arklove@qq.com>
2021-08-12 14:48:14 +0800

    docs: update changelog.js for 1.15.1

--- views/changelog.js
+++ views/changelog.js
@@ -2,18 +2,24 @@ export default {
   feature: [
     {
       text: {
-        en: 'Improve GitHub mirror feature',
-        zh_CN: '优化GitHub镜像加速功能',
+        en: 'Support Gogs',
+        zh_CN: '终于支持了Gogs，目前仅有懒加载模式',
+      },
+    },
+    {
+      text: {
+        en: 'Disable clipboard copy for GitHub',
+        zh_CN: '禁用复制模式，GitHub已有该功能',
       },
     },
   ],
   fix: [
     {
       text: {
-        en: 'Fix GitHub notifications problem',
-        zh_CN: '修复GitHub通知问题',
+        en: 'Fix download mirror url for GitHub',
+        zh_CN: '修复GitHub中镜像加速地址问题',
       },
     },
   ],
-  version: '1.14.0',
+  version: '1.15.1',
 };
