f08e70263ade4a7dddbb3f41dc1e21a9d3e22f40
neo <arklove@qq.com>
2022-01-21 17:37:34 +0800

    docs: update changelog.js

--- views/changelog.js
+++ views/changelog.js
@@ -1,25 +1,12 @@
 export default {
-  feature: [
-    {
-      text: {
-        en: 'Add option for file download and size feature',
-        zh_CN: '文件列表的下载和文件大小功能添加配置项，默认开启',
-      },
-    },
-    {
-      text: {
-        en: 'New toggle button UI',
-        zh_CN: '新的切换按钮UI',
-      },
-    },
-  ],
+  feature: [],
   fix: [
     {
       text: {
-        en: 'Fix dark mode style problems',
-        zh_CN: '修复黑暗模式样式问题',
+        en: 'Add prefix for download class',
+        zh_CN: '修复Gitee下载按钮问题',
       },
     },
   ],
-  version: '1.17.0',
+  version: '1.17.1',
 };
